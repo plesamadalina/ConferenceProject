35dfe8a4c4df943323ac489d45f6ef167e70657c
Madalina Plesa <madalina.plesa@totalsoft.ro>
2024-09-06 11:26:04 +0300

    dayThreeReact

--- src/features/conference/list/components/ConferenceItem.js
+++ src/features/conference/list/components/ConferenceItem.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useEmail } from 'hooks/useEmail'
+import ConferenceTitle from './ConferenceTitle'
+import { Card } from '@totalsoft/rocket-ui'
 import ConferenceSubtitle from './ConferenceSubtitle'
 import ConferenceContent from './ConferenceContent'
-import { Card } from '@totalsoft/rocket-ui'
 
 const ConferenceItem = props => {
   const { conference } = props
-  const { name, speakers, location } = conference
+  const { name, organizerEmail, speakers, location } = conference
   const speaker = speakers.find(speaker => speaker.isMainSpeaker)
 
+  const [email] = useEmail()
+  // destructure the "organizerEmail" from the conference
+  // compare the two emails and choose a title
+  const title = email.toUpperCase() === organizerEmail.toUpperCase() ? <ConferenceTitle title={name} /> : name
+
   return (
-    <Card title={name} subheader={<ConferenceSubtitle speaker={speaker} location={location} />}>
+    <Card title={title} subheader={<ConferenceSubtitle speaker={speaker} location={location} />}>
       <ConferenceContent conference={conference} />
     </Card>
   )
--- src/features/conference/list/components/ConferenceListContainer.js
+++ src/features/conference/list/components/ConferenceListContainer.js
@@ -1,15 +1,34 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useLayoutEffect, useEffect } from 'react'
 import ConferenceFilters from './ConferenceFilters'
 import conferences from 'utils/mocks/conferences'
 import { FakeText } from '@totalsoft/rocket-ui'
 import ConferenceList from './ConferenceList'
 import { generateDefaultFilters } from 'utils/functions'
+import { useTranslation } from 'react-i18next'
+import ConferenceHeader from 'features/conference/ConferenceHeader'
+import { IconButton } from '@totalsoft/rocket-ui'
+import { useHeader } from 'providers/AreasProvider'
 
 const ConferenceListContainer = () => {
+  const { t } = useTranslation()
   const { data, loading } = { data: conferences, loading: false } // don't worry about it! it will make a lot more sense after GraphQL
   const [filters, setFilters] = useState(generateDefaultFilters())
   const handleApplyFilters = useCallback(filters => setFilters(filters), [])
 
+  const [, setHeader] = useHeader()
+  //..
+
+  useLayoutEffect(() => () => setHeader(null), [setHeader])
+
+  useEffect(() => {
+    setHeader(
+      <ConferenceHeader
+        title={t('NavBar.Conferences')}
+        actions={<IconButton type='add' key='addButton' title={t('General.Buttons.AddConference')} />}
+      />
+    )
+  }, [setHeader, t])
+  //..
   if (loading) {
     return <FakeText lines={10} />
   }
